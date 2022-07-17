36b1c49ecf18bdee199f74c26b3c11a53fe9884a
thebarbakov <94574341+thebarbakov@users.noreply.github.com>
2022-07-17 20:39:42 +0300

    fix

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,103 +1,131 @@
-class Api{
-    constructor(config){
-        this._urlRequest = config.urlRequest
-        this._headers = config.headers
-    }
+class Api {
+  constructor(config) {
+    this._urlRequest = config.urlRequest;
+    this._headers = config.headers;
+  }
 
-    _checkResponse(res) {
-        if (res.ok) {
-            return res.json()
-        }
-        return Promise.reject(`Error: ${res.status}`)
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
     }
+    return Promise.reject(`Error: ${res.status}`);
+  }
 
-    getUserInfo(){
-        return fetch(`${this._urlRequest}/users/me`, {
-            method: 'GET',
-            headers: this._headers,
-        })
-        .then( res => this._checkResponse(res))
-    }
+  getUserInfo() {
+    return fetch(`${this._urlRequest}/users/me`, {
+      method: "GET",
+      headers: {
+        ...this._headers,
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("userData")).token || "",
+      },
+    }).then((res) => this._checkResponse(res));
+  }
 
-    getInitialCards(){
-        return fetch(`${this._urlRequest}/cards`, {
-            method: 'GET',
-            headers: this._headers,
-        })
-        .then( res => this._checkResponse(res))
-    }
+  getInitialCards() {
+    return fetch(`${this._urlRequest}/cards`, {
+      method: "GET",
+      headers: {
+        ...this._headers,
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("userData")).token || "",
+      },
+    }).then((res) => this._checkResponse(res));
+  }
 
-    getInitialInfo(){
-        return Promise.all([this.getUserInfo(), this.getInitialCards()])
-    }
+  getInitialInfo() {
+    return Promise.all([this.getUserInfo(), this.getInitialCards()]);
+  }
 
-    setUserInfo(newUserInfo){
-        return fetch(`${this._urlRequest}/users/me`, {
-        method: 'PATCH',
-        headers: this._headers,
-        body: JSON.stringify(newUserInfo)
-        })
-        .then( res => {return this._checkResponse(res)})
-    }
+  setUserInfo(newUserInfo) {
+    return fetch(`${this._urlRequest}/users/me`, {
+      method: "PATCH",
+      headers: {
+        ...this._headers,
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("userData")).token || "",
+      },
+      body: JSON.stringify(newUserInfo),
+    }).then((res) => {
+      return this._checkResponse(res);
+    });
+  }
 
-    setUserAvatar(newAvatar){
-        return fetch(`${this._urlRequest}/users/me/avatar`, {
-        method: 'PATCH',
-        headers: this._headers,
-        body: JSON.stringify(newAvatar)
-        })
-        .then( res => {return this._checkResponse(res)})
-    }
+  setUserAvatar(newAvatar) {
+    return fetch(`${this._urlRequest}/users/me/avatar`, {
+      method: "PATCH",
+      headers: {
+        ...this._headers,
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("userData")).token || "",
+      },
+      body: JSON.stringify(newAvatar),
+    }).then((res) => {
+      return this._checkResponse(res);
+    });
+  }
 
-    addCard(cardData){
-        return fetch(`${this._urlRequest}/cards`, {
-        method: 'POST',
-        headers: this._headers,
-        body: JSON.stringify(cardData)
-        })
-        .then( res => {return this._checkResponse(res)})
-    }
+  addCard(cardData) {
+    return fetch(`${this._urlRequest}/cards`, {
+      method: "POST",
+      headers: {
+        ...this._headers,
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("userData")).token || "",
+      },
+      body: JSON.stringify(cardData),
+    }).then((res) => {
+      return this._checkResponse(res);
+    });
+  }
 
-    deleteCard(cardId){
-        return fetch(`${this._urlRequest}/cards/${cardId}`, {
-        method: 'DELETE',
-        headers: this._headers,
-        })
-        .then( res => {
-            return this._checkResponse(res)
-        })
-    }
+  deleteCard(cardId) {
+    return fetch(`${this._urlRequest}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: {
+        ...this._headers,
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("userData")).token || "",
+      },
+    }).then((res) => {
+      return this._checkResponse(res);
+    });
+  }
 
-    changeLikeStatus(cardId, isLiked){
-        if (!isLiked){
-            return fetch(`${this._urlRequest}/cards/${cardId}/likes`, {
-                method: 'PUT',
-                headers: this._headers,
-                })
-                .then(res => {
-                    return this._checkResponse(res)
-                })
-        } else {
-            return fetch(`${this._urlRequest}/cards/${cardId}/likes`, {
-                method: 'DELETE',
-                headers: this._headers,
-                })
-                .then(res => {
-                    return this._checkResponse(res)
-                })
-        }
+  changeLikeStatus(cardId, isLiked) {
+    if (!isLiked) {
+      return fetch(`${this._urlRequest}/cards/${cardId}/likes`, {
+        method: "PUT",
+        headers: {
+          ...this._headers,
+          Authorization:
+            "Bearer " + JSON.parse(localStorage.getItem("userData")).token ||
+            "",
+        },
+      }).then((res) => {
+        return this._checkResponse(res);
+      });
+    } else {
+      return fetch(`${this._urlRequest}/cards/${cardId}/likes`, {
+        method: "DELETE",
+        headers: {
+          ...this._headers,
+          Authorization:
+            "Bearer " + JSON.parse(localStorage.getItem("userData")).token ||
+            "",
+        },
+      }).then((res) => {
+        return this._checkResponse(res);
+      });
     }
-    
+  }
 }
 
-const api = new Api(
-    {
-        urlRequest: 'https://mesto.calsser.ru/api',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem("userData")).token || "",
-        }
-    }
-)
+const api = new Api({
+  urlRequest: "https://mesto.calsser.ru/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-export default api
+export default api;
